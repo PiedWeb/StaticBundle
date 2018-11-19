9ca14d47409015a2e58b37840541e3a5859a4204
Robin <contact@robin-d.fr>
2018-11-19 12:14:01 +0100

    Fix namespace for service

--- src/Service/StaticService.php
+++ src/Service/StaticService.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace PiedWeb\CMSBundle\Service;
+namespace PiedWeb\StaticBundle\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Filesystem\Filesystem;
